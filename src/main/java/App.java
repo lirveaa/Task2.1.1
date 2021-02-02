0c4242c454daf1f26882ade562be9b08bff1403f
Husen Mansurov <lirvess@gmail.com>
2021-02-02 21:37:34 +0900

    first com

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
-    }
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat_bean = (Cat) applicationContext.getBean("cat");
+        Cat cat_bean2 = (Cat) applicationContext.getBean("cat");
+        boolean isEqual =bean==bean2;
+        boolean isEqual2 = cat_bean==cat_bean2;
+        System.out.println(isEqual);
+        System.out.println(isEqual2);
+     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,18 @@
+public class Cat {
+    private int id;
+    private String name;
+    private int age;
+
+    public Cat(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
